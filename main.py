a722ad1e933bf806a9b543b0c54535caf716eddf
yzeng <yzeng33@my.bcit.ca>
2024-08-28 18:25:23 -0700

    Define the time range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import schedule
 import time as tm
+from datetime import datetime
 import pyautogui
 
 
@@ -12,7 +13,15 @@ def main():
     schedule.every(4).minutes.do(click)
 
     while True:
-        schedule.run_pending()
+        current_time = datetime.now().time()
+
+        # Define the time range (9 AM to 4 PM)
+        start_time = datetime.strptime("09:00", "%H:%M").time()
+        end_time = datetime.strptime("16:00", "%H:%M").time()
+
+        if start_time <= current_time <= end_time:
+            schedule.run_pending()
+
         tm.sleep(1)
 
 
